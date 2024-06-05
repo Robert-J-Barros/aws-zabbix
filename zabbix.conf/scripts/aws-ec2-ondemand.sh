94dfe4e434f5130abc95aa07b559c5cf13cc79e7
Robert-J-Barros <robert.junior.barros@gmail.com>
2024-06-05 19:15:10 -0300

    add all

--- zabbix.conf/scripts/aws-ec2-ondemand.sh
+++ zabbix.conf/scripts/aws-ec2-ondemand.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
+reserved_instance_count=$(aws ec2 describe-reserved-instances --query 'ReservedInstances[*]' --output json | jq '. | length')
 
-# Contar o número de instâncias spot em execução
-spot=$(aws ec2 describe-instances --region sa-east-1 --query 'Reservations[*].Instances[*].InstanceLifecycle' --filters Name=instance-state-name,Values=running | grep -v '\[' | grep -v '\]' | wc -l)
-
-# Contar o número de instâncias on-demand em execução
-instancias=$(aws ec2 describe-instances --region us-east-1 --query 'Reservations[*].Instances[*].InstanceId' --filters Name=instance-state-name,Values=running | grep -c '^i-')
-
-# Calcular o número de instâncias on-demand subtraindo o número de instâncias spot do total de instâncias
-ondemand=$((instancias - spot))
-
-# Imprimir o número de instâncias on-demand
-echo $ondemand
+echo  $reserved_instance_count
 
